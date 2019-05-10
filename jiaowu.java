a4d79d8973a536cd00b3b597c32f4ecec235fc79
Administrator <Administrator@MB504-44>
2019-05-10 14:54:50 +0800

    教务作业

---
+++ jiaowu.java
@@ -0,0 +1,16 @@
+ package demoProj;
+			public class jiaowu {
+				public static void main(String[] args) {
+					System.out.println("good!");
+				}
+}
+\src\student.java，源代码内容为：
+package demoProj;
+public class student {
+   public String name;
+   public String address;
+   
+   student(){
+	   
+   }
+}
